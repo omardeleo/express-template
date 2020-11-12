756663f1b94a25ce5ac71685fd74710443bc6a2e
Omar De Leo <omar.deleo@gmail.com>
2020-11-12 16:47:55 -0500

    Configure Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'obscure-user',
+    password: 'obscure-password',
+    database: 'app',
+    host: 'postgres',
+    port: 5432,
+    dialect: 'postgres'
+  },
+};
