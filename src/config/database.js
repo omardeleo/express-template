5dd93bd7bc16211bfc8d60d951c74471ba957e83
Omar De Leo <omar.deleo@gmail.com>
2020-11-12 21:04:34 -0500

    Add environment variables to express service

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 module.exports = {
   development: {
-    username: 'obscure-user',
-    password: 'obscure-password',
-    database: 'app',
-    host: 'postgres',
-    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres'
   },
 };
